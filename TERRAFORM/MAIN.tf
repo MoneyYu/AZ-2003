1eac861c055b5b59766b10a19a1405a7115840c4
Money Yu <abc12207@gmail.com>
2025-05-14 00:16:14 +0800

    fix(readme): 更新課程日期和ID，新增連結及標籤 fix(terraform): 新增demo資源組並更新標籤 fix(terraform): 為容器註冊表的地理複製添加環境標籤

--- TERRAFORM/MAIN.tf
+++ TERRAFORM/MAIN.tf
@@ -118,3 +118,12 @@ resource "azurerm_resource_group" "rg" {
     environment = local.group_name
   }
 }
+
+resource "azurerm_resource_group" "demo" {
+  name     = "Demo${var.group_postfix}"
+  location = local.location
+
+  tags = {
+    environment = local.group_name
+  }
+}
